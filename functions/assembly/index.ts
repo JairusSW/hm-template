5d962e7e0fd504f0b7997a5eaa20f1b0c73c0d2f
JairusSW <jairus.v.tanaka@outlook.com>
2024-05-15 22:11:55 -0700

    joke api

--- functions/assembly/index.ts
+++ functions/assembly/index.ts
@@ -1,6 +1,12 @@
 import { http, inference } from "@hypermode/functions-as";
+// make cool shit happen
 export function textClassifier(text: string): string {
   const modelName = "sentiment-classifier";
   const threshold: f32 = 0.5;
   return inference.classifyText(modelName, text, threshold);
+}
+
+export function randomJoke(): string {
+  console.log("Sending request to https://v2.jokeapi.dev/joke/Any")
+  return http.fetch("https://v2.jokeapi.dev/joke/Any").text();
 }
